f4bb53558c557ec67941cd786823ac47c74a95d2
pratz1988 <prathyusha.jaddu@gmail.com>
2020-08-31 12:03:58 -0400

    Updates

---
+++ precache-manifest.25712b9fa6def6f83cb37dcdaf07bfef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74c67bc818d166c8c4ae215818773da",
+    "url": "/kanban-hack/index.html"
+  },
+  {
+    "revision": "23e7947843ed4def8511",
+    "url": "/kanban-hack/static/css/main.7c2542cd.chunk.css"
+  },
+  {
+    "revision": "bd8b47a8114138b1239f",
+    "url": "/kanban-hack/static/js/2.48b0fe2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban-hack/static/js/2.48b0fe2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e7947843ed4def8511",
+    "url": "/kanban-hack/static/js/main.2c2fad39.chunk.js"
+  },
+  {
+    "revision": "363fd58d1ab2f28ea55c",
+    "url": "/kanban-hack/static/js/runtime-main.2ff9ceaa.js"
+  }
+]);
